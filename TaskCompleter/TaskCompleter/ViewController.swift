9a15d4f63c6eea55ed3419439791b0196bfb3863
JB184351 <justinbengtson@icloud.com>
2019-09-12 10:45:58 -0600

    First commit for project

---
+++ TaskCompleter/TaskCompleter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskCompleter
+//
+//  Created by Justin Bengtson on 9/11/19.
+//  Copyright © 2019 Justin Bengtson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
