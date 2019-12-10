fe854a7ce70ab1881ff083bb6d780f948a3b3338
JB184351 <justinbengtson@icloud.com>
2019-12-09 20:46:35 -0700

    Attempting to save whether a task is completed or not

--- TaskCompleter/TaskCompleter/DetailTaskViewController.swift
+++ TaskCompleter/TaskCompleter/DetailTaskViewController.swift
@@ -16,8 +16,23 @@ class DetailTaskViewController: UIViewController {
     
     @IBOutlet var taskLabel: UILabel!
     @IBOutlet var taskDetails: UITextView!
+    @IBOutlet var isTaskDoneLabel: UILabel!
+    @IBOutlet var isTaskDoneSwitch: UISwitch!
     var selectedTask: TaskCompleterModel?
     weak var delegate: DetailTaskDelegate?
+    let userdefaults = UserDefaults.standard
+    
+    var isTaskDone = false {
+        didSet {
+            if isTaskDoneSwitch.isOn == true {
+                isTaskDone = true
+                userdefaults.set(isTaskDoneSwitch.isOn, forKey: "switchValue")
+            } else {
+                isTaskDone = false
+                userdefaults.set(isTaskDoneSwitch.isOn, forKey: "switchValue")
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +44,7 @@ class DetailTaskViewController: UIViewController {
     private func setupDetailVIewUI() {
         taskLabel.text = selectedTask?.name
         taskDetails.text = selectedTask?.taskDetail
+        isTaskDoneLabel?.textColor = .black
         taskLabel.textColor = .black
         taskDetails.textColor = .black
         taskDetails.backgroundColor = .white
@@ -38,14 +54,24 @@ class DetailTaskViewController: UIViewController {
     
     @objc private func saveTaskDetails() {
         selectedTask?.taskDetail = taskDetails.text
+        isTaskCompleted()
         delegate?.didUpdate(task: selectedTask)
     }
+    
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
     
-    
-    
-
+    private func isTaskCompleted() {
+        if isTaskDone {
+            if var isDone = selectedTask {
+                isDone.taskCompleted = true
+                isTaskDoneSwitch.isOn = userdefaults.bool(forKey: "switchValue")
+            } else {
+                selectedTask?.taskCompleted = false
+                isTaskDoneSwitch.isOn = userdefaults.bool(forKey: "switchValue")
+            }
+        }
+    }
 }
